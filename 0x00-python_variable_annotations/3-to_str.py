67c4659a7c67efe9b9438b1a52442de75f3ea897
Mohammed Refat <mohammedrefat53@outlook.com>
2024-08-04 22:53:23 +0300

    task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""to_Str function module"""
+
+
+def to_str(n: float) -> str:
+    """return string  reprsentation of a float 
+    args:
+    n: float
+    return: str
+    """
+    return n.__str__
