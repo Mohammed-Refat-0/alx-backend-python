aeeab019ec52877e006bfce1f53ed7b5f43bc42d
Mohammed Refat <mohammedrefat53@outlook.com>
2024-08-04 22:33:50 +0300

    task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""floor function module"""
+
+
+def floor(n: float) -> int:
+    """type annotated 'floor' function
+    args:
+    n: float
+    return: int
+    """
+    return floor(n)
