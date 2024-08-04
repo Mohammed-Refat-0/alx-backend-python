b517fc3eb742a472c96718a94c5eac449ee65c7f
Mohammed Refat <mohammedrefat53@outlook.com>
2024-08-04 22:31:58 +0300

    task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""concat function module"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """type annotated concatination function
+    args:
+    str1: str
+    str2: str
+    return: str
+    """
+    return str1 + str2
