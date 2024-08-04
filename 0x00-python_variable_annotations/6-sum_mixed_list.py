abbf5179eecfb506e8dceec885298291c04294eb
Mohammed Refat <mohammedrefat53@outlook.com>
2024-08-04 23:53:55 +0300

    task 6-fix

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python3
-"""sum_mixed_list function module"""
-
+'''sum_mixed_list module.
+'''
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
-    """return float sum of list elements
-    args:
-    mxd_list: List
-    return: float
-    """
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    '''Computes the sum of a list of integers and floats
+    '''
     return float(sum(mxd_lst))
