7f6d0ff4272dc3dadad94ac9b0caee73601a0007
Mohammed Refat <mohammedrefat53@outlook.com>
2024-08-04 23:27:09 +0300

    task 5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""sum_list function module"""
+from typing import List
+
+
+def sum_list(input_list: List) -> float:
+    """return float sum of list elements
+    args:
+    input_list: List
+    return: float
+    """
+    return float(sum(input_list))
