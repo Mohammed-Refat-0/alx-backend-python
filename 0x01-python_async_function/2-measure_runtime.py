49e4f03f97e669075d5127919196a760d5185011
Mohammed Refat <mohammedrefat53@outlook.com>
2024-08-05 22:34:42 +0300

    task 2 -fix

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -16,5 +16,4 @@ def measure_time(n: int, max_delay: int) -> List[float]:
     """
     start_time = time.time()
     asyncio.run(wait_n(n, max_delay))
-    elapsed_time = time.time() - start_time
-    return (elapsed_time/n)
+    return (time.time() - start_time) / n
