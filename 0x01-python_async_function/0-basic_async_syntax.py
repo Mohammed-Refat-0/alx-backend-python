3e81b0de88db33be3b12ab9c44d5e201a0345bf1
Mohammed Refat <mohammedrefat53@outlook.com>
2024-08-05 21:38:38 +0300

    task 0 -fix

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
-"""wait_random module"""
-from random import uniform
-from asyncio import sleep
+'''
+    wait_random module
+'''
+
+import asyncio
+import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
-    """wait a random amount between 0 and max_delay seconds"""
-    delay = uniform(0, max_delay)
-    await sleep(delay)
+    """
+    waits for a random delay between 0 and max_delay
+    """
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
     return delay
